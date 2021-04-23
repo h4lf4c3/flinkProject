70f1e6c4e151334b8598d72bc3439d4eb89a3b66
h4lf4c3 <2440463775@qq.com>
2021-04-23 16:54:11 +0800

    注释掉测试传输json格式数据

--- HotItems/src/main/scala/org/hjj/KafkaProducer.scala
+++ HotItems/src/main/scala/org/hjj/KafkaProducer.scala
@@ -1,13 +1,17 @@
 package org.hjj
 
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
 import java.util.Properties
 
+import com.alibaba.fastjson.JSONObject
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
 
 object KafkaProducer {
   def main(args: Array[String]): Unit = {
 //    writeToKafka("HotItems")
-    writeToKafka("user_behavior")
+    writeToKafka("Behavior")
   }
 
   def writeToKafka(topic: String): Unit = {
@@ -21,11 +25,19 @@ object KafkaProducer {
     // 从本地读取文件
 //    val bufferedSource = io.Source.fromFile("D:\\Code\\javaCode\\UserBehaviorBaseFlink\\HotItems\\src\\main\\resources\\UserBehavior.csv")
     val bufferedSource = io.Source.fromFile("D:\\Code\\javaCode\\data\\UserBehavior\\UserBehavior.csv")
+    val json = new JSONObject()
     for (line <- bufferedSource.getLines()) {
+//      val dataArray = line.split(",")
+//      json.put("user_id",dataArray(0))
+//      json.put("item_id",dataArray(1))
+//      json.put("category_id",dataArray(2))
+//      json.put("behavior",dataArray(3))
+//      json.put("ts",new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(dataArray(4).toLong *1000))
+//      json.put("app_time",(dataArray(4).toLong * 1000L).toString)
       println(line)
       val record = new ProducerRecord[String, String](topic, line) // 一行一行send到kafka
       producer.send(record)
-      Thread.sleep(500)
+      Thread.sleep(50)
     }
     producer.close()
   }
